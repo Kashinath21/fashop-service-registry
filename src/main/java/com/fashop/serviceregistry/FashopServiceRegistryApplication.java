9f6493b1bfa533794dd26acc20669ca1e245bdbf
Kashinath Mirajkar <Kashinath.Mirajkar@trovicor.com>
2020-03-02 17:15:36 +0530

    initial commit

---
+++ src/main/java/com/fashop/serviceregistry/FashopServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.fashop.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashopServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashopServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fashop/serviceregistry/FashopServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fashop.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashopServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
